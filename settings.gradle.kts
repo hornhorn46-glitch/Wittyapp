7fbb00e70f5bf05cd8f9b1cdbbcaa21457fa0548
hornhorn46-glitch <hornhorn46@gmail.com>
2026-02-21 02:12:25 +0300

    Upgrade Android and Kotlin plugin versions
    Updated Android and Kotlin plugin versions in settings.gradle.kts.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,28 @@ pluginManagement {
         mavenCentral()
     }
     plugins {
-        // Android application plugin
-        id("com.android.application") version "8.3.1"
-        // Kotlin Android plugin
-        id("org.jetbrains.kotlin.android") version "1.9.21"
+        pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "9.1.0"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WittyApp"
+include(":app")
     }
 }
 
