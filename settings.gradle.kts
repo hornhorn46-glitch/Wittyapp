f3e486a5455afd6273918f43dced8111789814fc
hornhorn46-glitch <hornhorn46@gmail.com>
2026-02-21 02:19:16 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,11 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
-    }
-    plugins {
-        pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "9.1.0"
-        id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
+        id("com.android.application") version "8.6.0"
+        id("org.jetbrains.kotlin.android") version "1.9.22"
     }
 }
 
@@ -24,18 +18,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "WittyApp"
-include(":app")
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "WittyApp"
+rootProject.name = "Wittyapp"
 include(":app")
