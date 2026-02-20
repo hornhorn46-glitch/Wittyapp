571ccdc267feb5801eaca421a8de0658556eb722
hornhorn46-glitch <hornhorn46@gmail.com>
2026-02-21 02:56:28 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
 }
+
 
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.10.00")
