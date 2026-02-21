907d0c1087a6a095f58a33b7e99d8725e8b15947
hornhorn46-glitch <hornhorn46@gmail.com>
2026-02-21 19:01:32 +0300

    Update Models.kt

--- app/src/main/java/com/example/wittyapp/domain/Models.kt
+++ app/src/main/java/com/example/wittyapp/domain/Models.kt
@@ -2,7 +2,20 @@ package com.example.wittyapp.domain
 
 import java.time.Instant
 
-data class KpSample(val t: Instant, val kp: Double)
-data class WindSample(val t: Instant, val speed: Double, val density: Double?)
-data class MagSample(val t: Instant, val bx: Double?, val by: Double?, val bz: Double?)
-data class GraphPoint(val t: Instant, val v: Double)
+data class KpSample(
+    val t: Instant,
+    val kp: Double
+)
+
+data class WindSample(
+    val t: Instant,
+    val speed: Double,
+    val density: Double?
+)
+
+data class MagSample(
+    val t: Instant,
+    val bx: Double?,
+    val by: Double?,
+    val bz: Double?
+)
