683c793b2ebcf5be41913275f997d6ce9fa60892
hornhorn46-glitch <hornhorn46@gmail.com>
2026-02-21 03:24:36 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-plugins {
-    // Intentionally left empty. The actual plugins are configured in settings.gradle.kts and module build files.
-}
+// Root build file. Module-level config is in app/build.gradle.kts
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
