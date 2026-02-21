efd1c7ba29f4474ee3379427b257a8b2eacc6583
hornhorn46-glitch <hornhorn46@gmail.com>
2026-02-21 03:09:22 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,5 +64,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
